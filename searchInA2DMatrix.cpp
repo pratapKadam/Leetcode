058f16d3720790668b8f8dff1b931adff681e48d
Pratap Kadam <105987201+pratapKadam@users.noreply.github.com>
2024-07-02 22:41:15 +0530

    Create searchInA2DMatrix.cpp
    LeetcodeQuestion

---
+++ searchInA2DMatrix.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int start = 0;
+        int end = row*col -1;
+
+        int mid = start + (end - start)/2;
+
+        while(start <= end){
+            int element = matrix[mid / col][mid % col];
+
+            if(element == target){
+                return true;
+            }
+
+            if(target < element){
+                end = mid-1;
+            }
+
+            else{
+                start = start+ 1;
+            }
+            mid = start + (end - start)/2;
+        }
+        return false;
+    }
+};
